490adc04cb1b2e86c1d7c907d5332f138728a5c9
husainsyn <husain.syn@gmail.com>
2022-06-03 09:49:23 +0700

    Update-6 team & contact

--- javascript/code.js
+++ javascript/code.js
@@ -2,3 +2,13 @@ let tombol_about = document.querySelector('.tombol-about');
   tombol_about.addEventListener('click', () => {
   tombol_about.classList.toggle('flip');
 });
+
+let tombol_team = document.querySelector('.tombol-team');
+  tombol_team.addEventListener('click', () => {
+  tombol_team.classList.toggle('flip');
+});
+
+let tombol_kontak = document.querySelector('.tombol-kontak');
+  tombol_kontak.addEventListener('click', () => {
+  tombol_kontak.classList.toggle('flip');
+});
